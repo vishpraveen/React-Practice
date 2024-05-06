174439fc7737d9083ef4464ad41b2097e9e985aa
vishpraveen <vishpraveen007@gmail.com>
2024-05-06 21:29:56 +0530

    adding test components

---
+++ src/component/Message.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message({ name }) {
+    return (
+        <div>
+            <p>Hello Welcome, {name}!</p>
+        </div>
+    )
+}
+
+export default Message
